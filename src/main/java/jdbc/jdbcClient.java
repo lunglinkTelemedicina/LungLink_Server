fa2583642340446b038a8e5939b627ff75204fa3
Your Name <you@example.com>
2025-11-04 19:33:04 +0100

    jdbcclient

--- src/main/java/jdbc/jdbcClient.java
+++ src/main/java/jdbc/jdbcClient.java
@@ -1,15 +1,32 @@
 package jdbc;
 
+import db.DBConnection;
 import jdbcInterfaces.ClientManager;
 import pojos.Client;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
+import java.sql.Connection;
+
 
 public class jdbcClient implements ClientManager {
 
 
     @Override
     public void addClient(Client client) {
+
+        String sql = "INSERT INTO clients (username, fullname, age, sex) VALUES (?, ?, ?, ?)";
+        try (Connection conn = DBConnection.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+
+            ps.setString(1, c.getUsername());
+            ps.setString(2, c.getFullName());
+            ps.setInt(3, c.getAge());
+            ps.setString(4, c.getSex().toString());
+            ps.executeUpdate();
+
+        } catch (SQLException e) { e.printStackTrace(); }
 
     }
 
